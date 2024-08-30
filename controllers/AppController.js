e4fb63a865f34778fc553096f8757c7ac7b668b4
Mariem Ehab <mariem.ehab234@gmail.com>
2024-08-30 08:59:41 -0700

    Modified to pass eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,15 +4,15 @@ import dbClient from '../utils/db';
 class AppController {
   static getStatus(req, res) {
     res.status(200).send({
-      "redis": redisClient.isAlive(),
-      "db": dbClient.isAlive(),
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
   }
 
   static async getStats(req, res) {
     res.status(200).send({
-      "users": await dbClient.nbUsers(),
-      "files": await dbClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     });
   }
 }
