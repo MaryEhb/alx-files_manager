043c1935b031ae296bea05552a1b82cd920e29a8
Mariem Ehab <mariem.ehab234@gmail.com>
2024-08-30 04:49:57 -0700

    Task 1 working

--- utils/db.js
+++ utils/db.js
@@ -7,11 +7,11 @@ class DBClient {
     this.DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 
     const uri = `mongodb://${this.DB_HOST}:${this.DB_PORT}`;
+    this.connected = false;
 
-    MongoClient.connect(uri, {}, (err, client) => {
+    MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
       if (err) {
         console.error('Failed to connect to db:', err.message);
-        this.connected = false;
       } else {
         this.db = client.db(this.DB_DATABASE);
         this.connected = true;
