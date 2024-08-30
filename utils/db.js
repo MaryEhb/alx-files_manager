2d23fc7d9172ee1693f8ee1a7991ec7785d606e2
Mariem Ehab <mariem.ehab234@gmail.com>
2024-08-30 04:28:40 -0700

    Task 1 mongodb client still not working

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.DB_HOST = process.env.DB_HOST || 'localhost';
+    this.DB_PORT = process.env.DB_PORT || '27017';
+    this.DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+    const uri = `mongodb://${this.DB_HOST}:${this.DB_PORT}`;
+
+    MongoClient.connect(uri, {}, (err, client) => {
+      if (err) {
+        console.error('Failed to connect to db:', err.message);
+        this.connected = false;
+      } else {
+        this.db = client.db(this.DB_DATABASE);
+        this.connected = true;
+      }
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    try {
+      return await this.db.collection('users').countDocuments();
+    } catch (err) {
+      console.error('Couldn\'t get users\' number:', err);
+      return null;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      return await this.db.collection('files').countDocuments();
+    } catch (err) {
+      console.error('Couldn\'t get files\' number:', err);
+      return null;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
